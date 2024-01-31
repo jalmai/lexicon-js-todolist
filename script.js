5c31f52213bc915bbd7018555e13183a82ba8370
mingman <ingman.m@gmail.com>
2024-01-31 14:34:52 +0100

    Todoitem class

---
+++ script.js
@@ -0,0 +1,14 @@
+class todoItem {
+  constructor(description, completed, timestamp) {
+    this.description = description;
+    this.completed = completed;
+    this.timestamp = timestamp;
+  }
+}
+// let a = new todoItem("Detta är ett test", false, "2024-01-31 12:33");
+// let b = new todoItem("Detta är ett test 2", false, "2024-01-31 12:33");
+// let c = new todoItem("Detta är ett test 3", false, "2024-01-31 12:33");
+// let d = new todoItem("Detta är ett test 4", false, "2024-01-31 12:33");
+// let e = new todoItem("Detta är ett test 5", false, "2024-01-31 12:33");
+
+// console.log(a);
