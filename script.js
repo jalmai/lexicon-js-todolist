baf0a27c4c513115d33e9ae08866a7be2b22a26b
mingman <ingman.m@gmail.com>
2024-01-31 17:28:01 +0100

    articles can be created

--- script.js
+++ script.js
@@ -6,14 +6,18 @@ class todoItem {
   }
 }
 let a = new todoItem("Detta är ett test", false, "2024-01-31 12:33");
-console.log(a);
 
 let form = document.querySelector(".new-todo");
+let itemList = document.querySelector(".todo-list");
 let descriptionItem = document.querySelector(".description");
+console.log(itemList);
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let newTodo = new todoItem(descriptionItem.value);
-  console.log(newTodo);
+  let art = document.createElement("article");
+  art.setAttribute("class", "todo-article");
+  art.innerText = newTodo.description;
+  itemList.insertAdjacentElement("beforeend", art);
 });
 // let b = new todoItem("Detta är ett test 2", false, "2024-01-31 12:33");
 // let c = new todoItem("Detta är ett test 3", false, "2024-01-31 12:33");
